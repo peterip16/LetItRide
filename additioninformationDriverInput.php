8f23c56d65b5d10ac6485dbcb26403b670a12fe4
ErnestIp <ernieip1@yahoo.com>
2016-11-10 01:11:02 -0800

    More input validation. User can't leave values blank.

--- additioninformationDriverInput.php
+++ additioninformationDriverInput.php
@@ -7,9 +7,26 @@
 
 
 <?php  session_start();
-require('php\connection.php');
+require('connection.php');
 
 if (isset($_POST['driverslicense']) && isset($_POST['licenseplate']) && isset($_POST['carmodel']) && isset($_POST['routingnumber']) && isset($_POST['bankaccountnumber'])){
+
+
+
+$con=mysqli_connect("localhost","root","","letitridesystem");
+
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  else{
+	  echo "Connection success ";
+  }
+
+
+//$driverslicense = $licenseplate = $carmodel = $routingnumber = $bankaccountnumber = "";
+
+
 
 $driverslicense = $_POST['driverslicense'];
 $licenseplate = $_POST['licenseplate'];
@@ -21,12 +38,73 @@ $userID = $_SESSION['UserID'];
 
 
 
+$a = true;
 
-$query = "SELECT * FROM `dit` WHERE Driverlicense='$driverslicense' and Licenseplate='$licenseplate' and Carmodel = '$carmodel' and Routingnumber = '$routingnumber' and Bankaccountnumber = '$bankaccountnumber'"; //Checking the values are existing in the database or not
 
-$result = mysqli_query($mysqli, $query);
+if($a)
+{
+if(empty($driverslicense))
+{
+  $driverslicense1 = "driverslicense";
+}
+else
+{
+  $driverslicense = "";
+}
+if(empty($licenseplate))
+{
+  $licenseplate1 = "licenseplate";
+}
+else
+{
+  $licenseplate = "";
+}
+if(empty($carmodel))
+{
+  $carmodel1 = "carmodel";
+}
+else
+{
+  $carmodel = "";
+}
+if(empty($routingnumber))
+{
+  $routingnumber1= "routingnumber";
+}
+else
+{
+  $routingnumber = "";
+}
+if(empty($bankaccountnumber))
+{
+  $bankaccountnumber1 = "bankaccountnumber";
+}
+else
+{
+  $bankaccountnumber = "";
+}
+echo "<script type='text/javascript'>alert('Please enter your $driverslicense1  $licenseplate1  $carmodel1  $routingnumber1  $bankaccountnumber1!')</script>";
+  header( "refresh:0; url=additionalinformationpageDriver.html" );
+  exit;
+
+}
+
+
+
+
+
+
+
+
+//$query = "SELECT * FROM dit WHERE Driverlicense = '$driverslicense' and Licenseplate='$licenseplate' and Carmodel = '$carmodel' and Routingnumber = '$routingnumber' and Bankaccountnumber = '$bankaccountnumber'"; //Checking the values are existing in the database or not
+
+$query = "SELECT * FROM dit where userID = '$userID'";
+$result = mysqli_query($con, $query);
+
+echo "Query ";
 
  if (mysqli_num_rows($result) > 0){ 
+ echo "It already exists! ";
     $_SESSION['error'] = "Already exists!";
     echo "<script type='text/javascript'>alert('Already Exists! Please try again.')</script>";
   header( "refresh:0; url=additionalinformationpageDriver.html" );
@@ -34,19 +112,36 @@ $result = mysqli_query($mysqli, $query);
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 else{
    // $query = "INSERT INTO rut VALUES (NULL, '$email', '$password', '$name', NULL)";
-   // echo "qw";
+    echo "qw ";
     $query = "INSERT INTO dit (UserID, Driverlicense, Licenseplate, Carmodel, Routingnumber, Bankaccountnumber) VALUES ('$userID', '$driverslicense', '$licenseplate', '$carmodel', '$routingnumber', '$bankaccountnumber')";
-    $result = mysqli_query($mysqli, $query) or die(mysql_error());
-    $_SESSION['id'] = mysqli_insert_id($mysqli);
+    $result = mysqli_query($con, $query); // or die(mysql_error());
+	echo mysqli_error($con);
+    $_SESSION['id'] = mysqli_insert_id($con);
+	echo "The result is: $result ";
     if ($result){
-
+		echo "Query succeed";
       echo "<script type='text/javascript'>alert('Successfully Registered!')</script>";
   header( "refresh:0; url=driverHomepage.php" );
  
 
       }
+	  else{
+		  echo "Query failed ";
+	  }
     }
   }
   
